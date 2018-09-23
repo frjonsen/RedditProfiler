a882a657f6917217d3e6c20a3a33919419dd8d29
Fredrik Jonsén <fredrikjonsen@gmail.com>
2018-09-23 19:38:37 +0200

    Created project base

---
+++ src/application.kt
@@ -0,0 +1,40 @@
+package se.jonsen
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.content.*
+import com.fasterxml.jackson.databind.*
+import io.ktor.jackson.*
+import io.ktor.features.*
+import io.ktor.http.content.resources
+import io.ktor.http.content.static
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.DevelopmentEngine.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+fun Application.module() {
+    install(ContentNegotiation) {
+        jackson {
+            enable(SerializationFeature.INDENT_OUTPUT)
+        }
+    }
+
+    routing {
+        get("/") {
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+
+        // Static feature. Try to access `/static/ktor_logo.svg`
+        static("/static") {
+            resources("static")
+        }
+
+        get("/json/jackson") {
+            call.respond(mapOf("hello" to "world"))
+        }
+    }
+}
+
